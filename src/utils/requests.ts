bbf544d6503303688c4f779cfa413a5f1a75cad4
mmorbel-lunchbox <mustafamorbel@gmail.com>
2023-08-27 19:49:38 +0300

    https fix

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,6 +1,6 @@
 import type { Movie, MovieSearchResult } from "types";
 const apikey = "a3b9f155";
-const apiUrl = "http://www.omdbapi.com/";
+const apiUrl = "https://www.omdbapi.com/";
 
 export const fetchMovies = async (
   searchTerm: string,
@@ -20,9 +20,7 @@ export const fetchMovies = async (
 
 export const fetchMovie = async (imdbID: string): Promise<Movie> => {
   try {
-    const response = await fetch(
-      `${apiUrl}?apikey=${apikey}&i=${imdbID}&plot=full`
-    );
+    const response = await fetch(`${apiUrl}?apikey=${apikey}&i=${imdbID}&plot=full`);
     const data = await response.json();
     return data;
   } catch (error) {
