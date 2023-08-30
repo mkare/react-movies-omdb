917d1a00b9b44e533449a16074b651488149750d
mmorbel-lunchbox <mustafamorbel@gmail.com>
2023-08-30 17:29:49 +0300

    searchbar bugfix

--- src/components/feature/MovieSearchBar.tsx
+++ src/components/feature/MovieSearchBar.tsx
@@ -2,23 +2,33 @@ import { Container, Input, Button } from "@components/base";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import Search from "@assets/Search";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 
 type MovieSearchBarProps = {
   onSubmit: (values: { search: string }) => void;
 };
 
 const MovieSearchBar: React.FC<MovieSearchBarProps> = ({ onSubmit }) => {
+  const searchTerm = useSelector(
+    (state: RootState) => state.movies.searchTerm || "Pokemon"
+  );
+
   const formik = useFormik({
     initialValues: {
-      search: "Pokemon",
+      search: searchTerm,
     },
     validationSchema: Yup.object({
-      search: Yup.string().required("Required"),
+      search: Yup.string(),
     }),
     onSubmit: (values) => {
       onSubmit(values);
     },
   });
+
+  const handleFocus = () => {
+    formik.setFieldValue("search", "");
+  };
 
   return (
     <Container>
@@ -28,11 +38,16 @@ const MovieSearchBar: React.FC<MovieSearchBarProps> = ({ onSubmit }) => {
           containerClassName="flex-grow mr-2"
           name="search"
           onChange={formik.handleChange}
+          onFocus={handleFocus}
+          value={formik.values.search}
         />
         <Button variant="secondary" size="small" type="submit" icon={<Search />}>
           Search
         </Button>
       </form>
+      {formik.touched.search && formik.errors.search ? (
+        <div className="text-red-500 text-sm italic">{formik.errors.search}</div>
+      ) : null}
     </Container>
   );
 };
